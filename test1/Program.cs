19f088da1766aaab823a0f613fe5f2c0cfe824d4
work <wps7712295@gmail.com>
2020-03-13 20:27:45 +0800

    加入專案檔案。

---
+++ test1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int hp = 10;
+            while (true)
+            {
+                if (hp <= 0)
+                {
+                    Console.WriteLine("oof");
+                    break;
+                }
+                string input = Console.ReadLine();
+                int attacke = int.Parse(input);
+                hp = hp - attacke;
+                Console.WriteLine(hp);
+            }
+            Console.ReadLine();
+        }
+    }
+}
